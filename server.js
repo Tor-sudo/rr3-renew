bfe887b8d4ef172e9a8108a6be9af3487589ba30
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-09-04 16:10:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 #!/usr/bin/env node
 'use strict';
 
-
 import fastify from 'fastify';
 import { processRequest } from './src/proxy.js'; // Import the named export
 
 const app = fastify({ 
-  logger: true,
-  disableRequestLogging: true,
-  trustProxy: true // Enable trust proxy
+  logger: { level: 'warn' }, // Reduced logging level for performance
 });
 
 const PORT = process.env.PORT || 8080;
 
-// Set up the route
-app.get('/', async (request, reply) => {
-  return processRequest(request, reply);
-});
+// Route directly linked to processRequest
+app.get('/', processRequest);
 
 // Start the server
 const start = async () => {
   try {
     await app.listen({ host: '0.0.0.0', port: PORT });
-    console.log(`Listening on ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
