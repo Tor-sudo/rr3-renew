dda2e833cba9f29ea4d7d433125461d0c938c35a
Tor-sudo <fhhhjgh567@tutamail.com>
2024-09-27 12:10:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
 
 import fastify from 'fastify';
 import { processRequest } from './src/proxy.js'; // Import the named export
+import http2 from 'http2';
 
 const app = fastify({ 
   logger: false, // Reduced logging level for performance
-  trustProxy: true        // Enable trust proxy for reverse proxies
+  trustProxy: true,// Enable trust proxy for reverse proxies
+  http2: true // Enable HTTP/2 support
 });
 
 const PORT = process.env.PORT || 8080;
