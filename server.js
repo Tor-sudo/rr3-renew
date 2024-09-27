b498a73ab360b8d67cb0a179e8fba621b70dd060
Tor-sudo <fhhhjgh567@tutamail.com>
2024-09-27 20:52:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { processRequest } from './src/proxy.js'; // Import the named export
 
 const app = fastify({ 
   logger: false, // Reduced logging level for performance
-  trustProxy: true,// Enable trust proxy for reverse proxies
+  trustProxy: true// Enable trust proxy for reverse proxies
 });
 
 const PORT = process.env.PORT || 8080;
